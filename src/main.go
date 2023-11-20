5e6099c64936b70343386bf868daffe94cf1f60e
Mike Taylor <mike@indexdata.com>
2023-11-20 15:24:03 +0000

    Add code to connect to the reporting database

--- src/main.go
+++ src/main.go
@@ -5,6 +5,14 @@ import "fmt"
 import "github.com/indexdata/foliogo"
 
 
+func exitIfError(err error, exitStatus int, message string) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %s: %s\n", os.Args[0], message, err)
+		os.Exit(exitStatus)
+	}
+}
+
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Fprintln(os.Stderr, "Usage:", os.Args[0], "<configFile.json>")
@@ -12,23 +20,17 @@ func main() {
 	}
 
 	session, err := foliogo.NewDefaultSession()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: FOLIO session creation failed: %s\n", os.Args[0], err)
-		os.Exit(2)
-	}
+	exitIfError(err, 2, "FOLIO session creation failed")
 
 	cfg, server := MakeConfiguredServer(os.Args[1], ".", session)
-
-	dbUrl, dbUser, _ /*dbPass*/, err := getDbInfo(session)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: cannot extract data from 'dbinfo': %s\n", os.Args[0], err)
-		os.Exit(2)
-	}
+	dbUrl, dbUser, dbPass, err := getDbInfo(session)
+	exitIfError(err, 3, "cannot extract data from 'dbinfo'")
 	server.Log("db", "url=" + dbUrl + ", user=" + dbUser)
 
+	err = server.connectDb(dbUrl, dbUser, dbPass)
+	exitIfError(err, 4, "cannot connected to DB")
+	server.Log("db", "connected to DB", dbUrl)
+
 	err = server.launch(cfg.Listen.Host + ":" + fmt.Sprint(cfg.Listen.Port))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: cannot create HTTP server: %s\n", os.Args[0], err)
-		os.Exit(3)
-	}
+	exitIfError(err, 5, "cannot launch HTTP server")
 }
