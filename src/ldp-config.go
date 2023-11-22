a94f9e604b5fba3f496412913db3f758c7613f74
Mike Taylor <mike@indexdata.com>
2023-11-22 15:56:59 +0000

    Towards supporting PUT /ldp/config/{key}
    Not yet working due to problems with JSON-format value

--- src/ldp-config.go
+++ src/ldp-config.go
@@ -1,10 +1,12 @@
 // handle the /ldp/config and /ldp/config/{key} endpoints
 package main
 
+import "io"
 import "fmt"
 import "strings"
 import "net/http"
 import "encoding/json"
+import "github.com/google/uuid"
 import "github.com/indexdata/foliogo"
 
 
@@ -12,9 +14,10 @@ import "github.com/indexdata/foliogo"
 // "General" here means not knowing the structure of the value
 
 type settingsItemGeneral struct {
+	Id string `json:"id"`
+	Scope string `json:"scope"`
 	Key string `json:"key"`
 	Value interface{}
-	// We don't care about id (irrelevant) or scope (always "ui-ldp.admin")
 }
 
 type settingsResultInfo struct {
@@ -39,7 +42,7 @@ type configItem struct {
 
 // The /ldp/config endpoint only supports GET, with no URL parameters
 func handleConfig(w http.ResponseWriter, req *http.Request, server *ModReportingServer) {
-	bytes, err := server.folioSession.Fetch(`settings/entries?query=scope=="ui-ldp.admin"`, foliogo.RequestParams{})
+	bytes, err := server.folioSession.Fetch0(`settings/entries?query=scope=="ui-ldp.admin"`)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "could not fetch from mod-settings: %s", err)
@@ -83,30 +86,42 @@ func handleConfig(w http.ResponseWriter, req *http.Request, server *ModReporting
 }
 
 
-// The /ldp/config/{key} endpoint only GET and PUT
 func handleConfigKey(w http.ResponseWriter, req *http.Request, server *ModReportingServer) {
-	key := strings.Replace(req.URL.Path, "/ldp/config/", "", 1)
-	path := `settings/entries?query=scope=="ui-ldp.admin"+and+key=="` + key + `"`
-	bytes, err := server.folioSession.Fetch(path, foliogo.RequestParams{})
+	err := underlyingHandleConfigKey(w, req, server)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "could not fetch from mod-settings: %s", err)
-		return
+		fmt.Fprintln(w, err.Error())
+	}
+}
+
+
+// The /ldp/config/{key} endpoint only supports GET and PUT
+func underlyingHandleConfigKey(w http.ResponseWriter, req *http.Request, server *ModReportingServer) error {
+	key := strings.Replace(req.URL.Path, "/ldp/config/", "", 1)
+	var bytes []byte
+	var err error
+	var verb string
+
+	if (req.Method == "PUT") {
+		bytes, err = writeConfigKey(w, req, server, key)
+		verb = "write to"
+	} else {
+		// Assume GET
+		bytes, err = server.folioSession.Fetch0(`settings/entries?query=scope=="ui-ldp.admin"+and+key=="` + key + `"`)
+		verb = "read from"
+	}
+	if err != nil {
+		return fmt.Errorf("could not %s mod-settings: %s", verb, err)
 	}
 
 	var r settingsResponseGeneral
 	err = json.Unmarshal(bytes, &r)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "could not deserialize JSON from mod-settings: %s", err)
-		return
+		return fmt.Errorf("could not deserialize JSON from mod-settings: %s", err)
 	}
 
 	if r.ResultInfo.TotalRecords < 1 {
-		// No config entry of that name
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "no config item with key '%s'", key)
-		return
+		return fmt.Errorf("no config item with key '%s'", key)
 	}
 
 	item := r.Items[0]
@@ -119,11 +134,50 @@ func handleConfigKey(w http.ResponseWriter, req *http.Request, server *ModReport
 
 	bytes, err = json.Marshal(config)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "could not serialize JSON: %s", err)
-		return
+		return fmt.Errorf("could not serialize JSON: %s", err)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	_, _ = w.Write(bytes)
+	_, err = w.Write(bytes)
+	return nil
+}
+
+
+func writeConfigKey(w http.ResponseWriter, req *http.Request, server *ModReportingServer, key string) ([]byte, error) {
+	bytes, err := io.ReadAll(req.Body)
+	if err != nil {
+		return nil, fmt.Errorf("could not read HTTP request body: %s", err)
+	}
+
+	var item configItem
+	err = json.Unmarshal(bytes, &item)
+	if err != nil {
+		return nil, fmt.Errorf("could not deserialize JSON from body: %s", err)
+	}
+	fmt.Printf("deserialized config JSON %+v\n", item)
+	fmt.Println("item.value =", item.Value.(string))
+
+	// XXX For now, assume we're creating a new entry
+	// But we need to check if there is already an entry with this key
+
+	id, err := uuid.NewRandom()
+	if err != nil {
+		return nil, fmt.Errorf("could not generate v4 UUID: %s", err)
+	}
+
+	settingsItem := settingsItemGeneral{
+		Id: id.String(),
+		Scope: "ui-ldp.admin",
+		Key: key,
+		Value: item.Value,
+	}
+	bytes, err = json.Marshal(settingsItem)
+	if err != nil {
+		return nil, fmt.Errorf("could not serialize JSON: %s", err)
+	}
+	fmt.Printf("serialized mod-settings JSON %+v\n", settingsItem)
+	return server.folioSession.Fetch("settings/entries", foliogo.RequestParams{
+		Method: "POST",
+		Body: string(bytes),
+	})
 }
