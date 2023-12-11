f349c05957df9ab1189c47fc7f8dda423bf24859
Mike Taylor <mike@indexdata.com>
2023-12-11 17:08:25 +0000

    Add test-case for present-but-malformed config file

--- src/config-file_test.go
+++ src/config-file_test.go
@@ -7,18 +7,23 @@ import "gotest.tools/assert"
 func Test_readConfig(t *testing.T) {
 	t.Run("non-existent config file", func(t *testing.T) {
 		cfg, err := readConfig("/no/such/file.json")
-		assert.Error(t, err, "open /no/such/file.json: no such file or directory")
 		var nilConfig *config
 		assert.Equal(t, cfg, nilConfig)
+		assert.Error(t, err, "open /no/such/file.json: no such file or directory")
+	})
+
+	t.Run("malformed config file", func(t *testing.T) {
+		cfg, err := readConfig("../etc/not-json.txt")
+		var nilConfig *config
+		assert.Equal(t, cfg, nilConfig)
+		assert.Error(t, err, "invalid character 'T' looking for beginning of value")
 	})
 
 	t.Run("well-known config file", func(t *testing.T) {
-		cfg, err := readConfig("../etc/config.json")
+		cfg, err := readConfig("../etc/silent.json")
 		assert.NilError(t, err)
 		assert.DeepEqual(t, cfg, &config{
-			Logging: loggingConfig{
-				Categories: "config,listen,path,error",
-			},
+			Logging: loggingConfig{},
 			Listen: listenConfig{
 				Host: "0.0.0.0",
 				Port: 12369,
