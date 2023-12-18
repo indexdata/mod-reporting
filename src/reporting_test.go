751fc45ab1f9455d73e7d426a35640e4834498ba
Mike Taylor <mike@indexdata.com>
2023-12-18 17:46:13 +0000

    Switch to table-based testing

--- src/reporting_test.go
+++ src/reporting_test.go
@@ -1,10 +1,56 @@
 package main
 
 import "io"
+import "strings"
 import "testing"
 import "github.com/stretchr/testify/assert"
 import "github.com/pashagolub/pgxmock/v3"
 import "net/http/httptest"
+
+
+var reportingTests []testT = []testT{
+	{
+		name: "retrieve list of tables",
+		path: "/ldp/db/tables",
+		establishMock: func(data interface{}) error {
+			mock := data.(pgxmock.PgxPoolIface)
+			mock.ExpectQuery("SELECT schema_name, table_name FROM metadb.base_table").WillReturnRows(
+				pgxmock.NewRows([]string{"schema_name", "table_name"}).
+					AddRow("folio_inventory", "records_instances").
+					AddRow("folio_inventory", "holdings_record"))
+			return nil
+		},
+		function: handleTables,
+		expected: `[{"schemaName":"folio_inventory","tableName":"records_instances"},{"schemaName":"folio_inventory","tableName":"holdings_record"}]`,
+	},
+	{
+		name: "list of columns without table",
+		path: "/ldp/db/columns?schema=folio_users",
+		function: handleColumns,
+		errorstr: "must specify both schema and table",
+	},
+	{
+		name: "list of columns without schema",
+		path: "/ldp/db/columns?table=users",
+		function: handleColumns,
+		errorstr: "must specify both schema and table",
+	},
+	{
+		name: "retrieve list of columns",
+		path: "/ldp/db/columns?schema=folio_users&table=users",
+		establishMock: func(data interface{}) error {
+			mock := data.(pgxmock.PgxPoolIface)
+			mock.ExpectQuery(`SELECT`).
+				WithArgs("folio_users", "users", "data").
+				WillReturnRows(pgxmock.NewRows([]string{"column_name", "data_type", "ordinal_position", "table_schema", "table_name"}).
+					AddRow("id", "uuid", "6", "folio_users", "users").
+					AddRow("creation_date", "timestamp without time zone", "8", "folio_users", "users"))
+			return nil
+		},
+		function: handleColumns,
+		expected: `{"columnName":"id","data_type":"uuid","tableSchema":"folio_users","tableName":"users","ordinalPosition":"6"},{"columnName":"creation_date","data_type":"timestamp without time zone","tableSchema":"folio_users","tableName":"users","ordinalPosition":"8"}]`,
+	},
+}
 
 
 func Test_handleTables(t *testing.T) {
@@ -16,6 +62,7 @@ func Test_handleTables(t *testing.T) {
 	assert.Nil(t, err)
 	session, err := NewModReportingSession(mrs, baseUrl, "dummyTenant")
 	assert.Nil(t, err)
+
 
 	t.Run("unable to obtain DB connection", func(t *testing.T) {
 		req := httptest.NewRequest("GET", "http://example.com/dummy", nil)
@@ -31,58 +78,35 @@ func Test_handleTables(t *testing.T) {
 	defer mock.Close()
 	session.dbConn = mock
 
-	t.Run("retrieve list of tables", func(t *testing.T) {
-		rows := pgxmock.NewRows([]string{"schema_name", "table_name"}).
-			AddRow("folio_inventory", "records_instances").
-			AddRow("folio_inventory", "holdings_record")
-		mock.ExpectQuery("SELECT schema_name, table_name FROM metadb.base_table").WillReturnRows(rows)
+	for _, test := range reportingTests {
+		t.Run(test.name, func(t *testing.T) {
+			method := "GET"
+			var reader io.Reader
+			if test.sendData != "" {
+				method = "POST"
+				reader = strings.NewReader(test.sendData)
+			}
+			req := httptest.NewRequest(method, baseUrl + test.path, reader)
 
-		req := httptest.NewRequest("GET", "http://example.com/dummy", nil)
-		w := httptest.NewRecorder()
-		err = handleTables(w, req, session)
-		resp := w.Result()
+			if test.establishMock != nil {
+				err = test.establishMock(mock)
+				assert.Nil(t, err)
+			}
 
-		assert.Nil(t, err)
-		assert.Equal(t, 200, resp.StatusCode)
-		body, _ := io.ReadAll(resp.Body)
-		assert.Equal(t, string(body), `[{"schemaName":"folio_inventory","tableName":"records_instances"},{"schemaName":"folio_inventory","tableName":"holdings_record"}]`)
-		assert.Nil(t, mock.ExpectationsWereMet(), "unfulfilled expections")
-	})
+			w := httptest.NewRecorder()
+			err = test.function(w, req, session)
+			resp := w.Result()
 
-	t.Run("list of columns without table", func(t *testing.T) {
-		req := httptest.NewRequest("GET", "http://example.com/dummy?schema=folio_users", nil)
-		w := httptest.NewRecorder()
-		err = handleColumns(w, req, session)
+			if test.errorstr == "" {
+				assert.Nil(t, err)
+				assert.Equal(t, resp.StatusCode, 200)
+				body, _ := io.ReadAll(resp.Body)
+				assert.Regexp(t, test.expected, string(body))
+			} else {
+				assert.ErrorContains(t, err, test.errorstr)
+			}
 
-		assert.ErrorContains(t, err, "must specify both schema and table")
-	})
-
-	t.Run("list of columns without schema", func(t *testing.T) {
-		req := httptest.NewRequest("GET", "http://example.com/dummy?table=users", nil)
-		w := httptest.NewRecorder()
-		err = handleColumns(w, req, session)
-
-		assert.ErrorContains(t, err, "must specify both schema and table")
-	})
-
-	t.Run("retrieve list of columns", func(t *testing.T) {
-		rows := pgxmock.NewRows([]string{"column_name", "data_type", "ordinal_position", "table_schema", "table_name"}).
-			AddRow("id", "uuid", "6", "folio_users", "users").
-			AddRow("creation_date", "timestamp without time zone", "8", "folio_users", "users")
-		mock.ExpectQuery(`SELECT`).
-			WithArgs("folio_users", "users", "data").
-			WillReturnRows(rows)
-
-		req := httptest.NewRequest("GET", "http://example.com/dummy?schema=folio_users&table=users", nil)
-		w := httptest.NewRecorder()
-		err = handleColumns(w, req, session)
-		resp := w.Result()
-
-		assert.Nil(t, err)
-		assert.Equal(t, 200, resp.StatusCode)
-		body, _ := io.ReadAll(resp.Body)
-		assert.Equal(t, `[{"columnName":"id","data_type":"uuid","tableSchema":"folio_users","tableName":"users","ordinalPosition":"6"},{"columnName":"creation_date","data_type":"timestamp without time zone","tableSchema":"folio_users","tableName":"users","ordinalPosition":"8"}]`, string(body))
-
-		assert.Nil(t, mock.ExpectationsWereMet(), "unfulfilled expections")
-	})
+			assert.Nil(t, mock.ExpectationsWereMet(), "unfulfilled expections")
+		})
+	}
 }
