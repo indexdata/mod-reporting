19e87a9855c726169347881ed8db11163a7bc39a
Mike Taylor <mike@indexdata.com>
2023-12-19 13:52:47 +0000

    Move lists of tests inside functions

--- src/ldp-config_test.go
+++ src/ldp-config_test.go
@@ -105,63 +105,63 @@ type testT struct {
 	useBadSession bool
 }
 
-var ldpConfigTests []testT = []testT{
-	{
-		name: "fetch all configs from table",
-		path: "/ldp/config",
-		function: handleConfig,
-		expected: `[{"key":"config","tenant":"dummyTenant","value":"v1"}]`,
-	},
-	{
-		name: "fetch single config",
-		path: "/ldp/config/dbinfo",
-		function: handleConfigKey,
-		expected: `{"key":"dbinfo","tenant":"dummyTenant","value":`,
-	},
-	{
-		name: "non-existent config",
-		path: "/ldp/config/not-there",
-		function: handleConfigKey,
-		errorstr: "no config item with key",
-	},
-	{
-		name: "fetch malformed config",
-		path: "/ldp/config/bad",
-		function: handleConfigKey,
-	        errorstr: "could not deserialize",
-	},
-	{
-		name: "translate non-string value",
-		path: "/ldp/config/non-string",
-		function: handleConfigKey,
-		expected: `{"key":"non-string","tenant":"dummyTenant","value":"{\\|"v3\\\":42}"}`,
-	},
-	{
-		name: "failure to reach mod-settings",
-		path: "/ldp/config/non-string",
-		function: handleConfig,
-	        errorstr: "could not fetch from mod-settings",
-		useBadSession: true,
-	},
-	{
-		name: "write a new config value",
-		path: "/ldp/config/foo",
-		sendData: `{"key":"foo","tenant":"xxx","value":"{\"user\":\"abc123\"}"}`,
-		function: handleConfigKey,
-		expected: "abc123",
-	},
-	{
-		name: "rewrite an existing config value",
-		path: "/ldp/config/dbinfo",
-		sendData: `{"key":"dbinfo","tenant":"xxx","value":"{\"user\":\"abc456\"}"}`,
-		function: handleConfigKey,
-		expected: "abc456",
-	},
-	// At this point it seems silly to laboriously chase each individual error case
-}
-
 
 func Test_handleConfig(t *testing.T) {
+	tests := []testT{
+		{
+			name: "fetch all configs from table",
+			path: "/ldp/config",
+			function: handleConfig,
+			expected: `[{"key":"config","tenant":"dummyTenant","value":"v1"}]`,
+		},
+		{
+			name: "fetch single config",
+			path: "/ldp/config/dbinfo",
+			function: handleConfigKey,
+			expected: `{"key":"dbinfo","tenant":"dummyTenant","value":`,
+		},
+		{
+			name: "non-existent config",
+			path: "/ldp/config/not-there",
+			function: handleConfigKey,
+			errorstr: "no config item with key",
+		},
+		{
+			name: "fetch malformed config",
+			path: "/ldp/config/bad",
+			function: handleConfigKey,
+			errorstr: "could not deserialize",
+		},
+		{
+			name: "translate non-string value",
+			path: "/ldp/config/non-string",
+			function: handleConfigKey,
+			expected: `{"key":"non-string","tenant":"dummyTenant","value":"{\\|"v3\\\":42}"}`,
+		},
+		{
+			name: "failure to reach mod-settings",
+			path: "/ldp/config/non-string",
+			function: handleConfig,
+			errorstr: "could not fetch from mod-settings",
+			useBadSession: true,
+		},
+		{
+			name: "write a new config value",
+			path: "/ldp/config/foo",
+			sendData: `{"key":"foo","tenant":"xxx","value":"{\"user\":\"abc123\"}"}`,
+			function: handleConfigKey,
+			expected: "abc123",
+		},
+		{
+			name: "rewrite an existing config value",
+			path: "/ldp/config/dbinfo",
+			sendData: `{"key":"dbinfo","tenant":"xxx","value":"{\"user\":\"abc456\"}"}`,
+			function: handleConfigKey,
+			expected: "abc456",
+		},
+		// At this point it seems silly to laboriously chase each individual error case
+	}
+
 	ts := MakeDummyModSettingsServer()
 	defer ts.Close()
 	baseUrl := ts.URL
@@ -171,7 +171,7 @@ func Test_handleConfig(t *testing.T) {
 	badSession, err := NewModReportingSession(nil, "x" + baseUrl, "dummyTenant")
 	assert.Nil(t, err)
 
-	for i, test := range ldpConfigTests {
+	for i, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			method := "GET"
 			var reader io.Reader
--- src/reporting_test.go
+++ src/reporting_test.go
@@ -8,136 +8,135 @@ import "github.com/pashagolub/pgxmock/v3"
 import "net/http/httptest"
 
 
-var reportingTests []testT = []testT{
-	{
-		name: "bad DB connection for tables",
-		useBadSession: true,
-		function: handleTables,
-		errorstr: "failed to connect",
-	},
-	{
-		name: "bad DB connection for columns",
-		path: "/ldp/db/columns?schema=folio_users&table=users",
-		useBadSession: true,
-		function: handleColumns,
-		errorstr: "failed to connect",
-	},
-	{
-		name: "retrieve list of tables",
-		path: "/ldp/db/tables",
-		establishMock: func(data interface{}) error {
-			mock := data.(pgxmock.PgxPoolIface)
-			mock.ExpectQuery("SELECT schema_name, table_name FROM metadb.base_table").WillReturnRows(
-				pgxmock.NewRows([]string{"schema_name", "table_name"}).
-					AddRow("folio_inventory", "records_instances").
-					AddRow("folio_inventory", "holdings_record"))
-			return nil
-		},
-		function: handleTables,
-		expected: `[{"schemaName":"folio_inventory","tableName":"records_instances"},{"schemaName":"folio_inventory","tableName":"holdings_record"}]`,
-	},
-	{
-		name: "list of columns without table",
-		path: "/ldp/db/columns?schema=folio_users",
-		function: handleColumns,
-		errorstr: "must specify both schema and table",
-	},
-	{
-		name: "list of columns without schema",
-		path: "/ldp/db/columns?table=users",
-		function: handleColumns,
-		errorstr: "must specify both schema and table",
-	},
-	{
-		name: "retrieve list of columns",
-		path: "/ldp/db/columns?schema=folio_users&table=users",
-		establishMock: func(data interface{}) error {
-			mock := data.(pgxmock.PgxPoolIface)
-			mock.ExpectQuery(`SELECT`).
-				WithArgs("folio_users", "users", "data").
-				WillReturnRows(pgxmock.NewRows([]string{"column_name", "data_type", "ordinal_position", "table_schema", "table_name"}).
-					AddRow("id", "uuid", "6", "folio_users", "users").
-					AddRow("creation_date", "timestamp without time zone", "8", "folio_users", "users"))
-			return nil
-		},
-		function: handleColumns,
-		expected: `{"columnName":"id","data_type":"uuid","tableSchema":"folio_users","tableName":"users","ordinalPosition":"6"},{"columnName":"creation_date","data_type":"timestamp without time zone","tableSchema":"folio_users","tableName":"users","ordinalPosition":"8"}]`,
-	},
-	{
-		name: "fail non-JSON query",
-		path: "/ldp/db/query",
-		sendData: "water",
-		function: handleQuery,
-		errorstr: "deserialize JSON",
-	},
-	{
-		name: "fail non-JSON query",
-		path: "/ldp/db/query",
-		sendData: `{}`,
-		function: handleQuery,
-		errorstr: "must have exactly one table",
-	},
-	{
-		name: "fail JSON query where tables is number",
-		path: "/ldp/db/query",
-		sendData: `{ "tables": 42 }`,
-		function: handleQuery,
-		errorstr: "cannot unmarshal number",
-	},
-	{
-		name: "fail JSON query where tables is string",
-		path: "/ldp/db/query",
-		sendData: `{ "tables": "water" }`,
-		function: handleQuery,
-		errorstr: "cannot unmarshal string",
-	},
-	{
-		name: "fail JSON query with 0 tables",
-		path: "/ldp/db/query",
-		sendData: `{ "tables": [] }`,
-		function: handleQuery,
-		errorstr: "must have exactly one table",
-	},
-	{
-		name: "fail JSON query with 2 tables",
-		path: "/ldp/db/query",
-		sendData: `{ "tables": [{}, {}] }`,
-		function: handleQuery,
-		errorstr: "must have exactly one table",
-	},
-	{
-		name: "fail JSON query where table is number",
-		path: "/ldp/db/query",
-		sendData: `{ "tables": [42] }`,
-		function: handleQuery,
-		errorstr: "cannot unmarshal number",
-	},
-	{
-		name: "fail JSON query where table is string",
-		path: "/ldp/db/query",
-		sendData: `{ "tables": ["water"] }`,
-		function: handleQuery,
-		errorstr: "cannot unmarshal string",
-	},
-	{
-		name: "simple query with dummy reslts",
-		path: "/ldp/db/query",
-		sendData: `{ "tables": [{ "schema": "folio", "tableName": "users" }] }`,
-		establishMock: func(data interface{}) error {
-			mock := data.(pgxmock.PgxPoolIface)
-			mock.ExpectQuery(`SELECT \* FROM "folio"."users"`).
-				WillReturnRows(pgxmock.NewRows([]string{"name", "email"}).
-					AddRow("mike", "mike@example.com").
-					AddRow("fiona", "fiona@example.com"))
-			return nil
-		},
-		function: handleQuery,
-		expected: `[{"email":"mike@example.com","name":"mike"},{"email":"fiona@example.com","name":"fiona"}]`,
-	},
-}
-
-
 func Test_handleTables(t *testing.T) {
+	tests := []testT{
+		{
+			name: "bad DB connection for tables",
+			useBadSession: true,
+			function: handleTables,
+			errorstr: "failed to connect",
+		},
+		{
+			name: "bad DB connection for columns",
+			path: "/ldp/db/columns?schema=folio_users&table=users",
+			useBadSession: true,
+			function: handleColumns,
+			errorstr: "failed to connect",
+		},
+		{
+			name: "retrieve list of tables",
+			path: "/ldp/db/tables",
+			establishMock: func(data interface{}) error {
+				mock := data.(pgxmock.PgxPoolIface)
+				mock.ExpectQuery("SELECT schema_name, table_name FROM metadb.base_table").WillReturnRows(
+					pgxmock.NewRows([]string{"schema_name", "table_name"}).
+						AddRow("folio_inventory", "records_instances").
+						AddRow("folio_inventory", "holdings_record"))
+				return nil
+			},
+			function: handleTables,
+			expected: `[{"schemaName":"folio_inventory","tableName":"records_instances"},{"schemaName":"folio_inventory","tableName":"holdings_record"}]`,
+		},
+		{
+			name: "list of columns without table",
+			path: "/ldp/db/columns?schema=folio_users",
+			function: handleColumns,
+			errorstr: "must specify both schema and table",
+		},
+		{
+			name: "list of columns without schema",
+			path: "/ldp/db/columns?table=users",
+			function: handleColumns,
+			errorstr: "must specify both schema and table",
+		},
+		{
+			name: "retrieve list of columns",
+			path: "/ldp/db/columns?schema=folio_users&table=users",
+			establishMock: func(data interface{}) error {
+				mock := data.(pgxmock.PgxPoolIface)
+				mock.ExpectQuery(`SELECT`).
+					WithArgs("folio_users", "users", "data").
+					WillReturnRows(pgxmock.NewRows([]string{"column_name", "data_type", "ordinal_position", "table_schema", "table_name"}).
+						AddRow("id", "uuid", "6", "folio_users", "users").
+						AddRow("creation_date", "timestamp without time zone", "8", "folio_users", "users"))
+				return nil
+			},
+			function: handleColumns,
+			expected: `{"columnName":"id","data_type":"uuid","tableSchema":"folio_users","tableName":"users","ordinalPosition":"6"},{"columnName":"creation_date","data_type":"timestamp without time zone","tableSchema":"folio_users","tableName":"users","ordinalPosition":"8"}]`,
+		},
+		{
+			name: "fail non-JSON query",
+			path: "/ldp/db/query",
+			sendData: "water",
+			function: handleQuery,
+			errorstr: "deserialize JSON",
+		},
+		{
+			name: "fail non-JSON query",
+			path: "/ldp/db/query",
+			sendData: `{}`,
+			function: handleQuery,
+			errorstr: "must have exactly one table",
+		},
+		{
+			name: "fail JSON query where tables is number",
+			path: "/ldp/db/query",
+			sendData: `{ "tables": 42 }`,
+			function: handleQuery,
+			errorstr: "cannot unmarshal number",
+		},
+		{
+			name: "fail JSON query where tables is string",
+			path: "/ldp/db/query",
+			sendData: `{ "tables": "water" }`,
+			function: handleQuery,
+			errorstr: "cannot unmarshal string",
+		},
+		{
+			name: "fail JSON query with 0 tables",
+			path: "/ldp/db/query",
+			sendData: `{ "tables": [] }`,
+			function: handleQuery,
+			errorstr: "must have exactly one table",
+		},
+		{
+			name: "fail JSON query with 2 tables",
+			path: "/ldp/db/query",
+			sendData: `{ "tables": [{}, {}] }`,
+			function: handleQuery,
+			errorstr: "must have exactly one table",
+		},
+		{
+			name: "fail JSON query where table is number",
+			path: "/ldp/db/query",
+			sendData: `{ "tables": [42] }`,
+			function: handleQuery,
+			errorstr: "cannot unmarshal number",
+		},
+		{
+			name: "fail JSON query where table is string",
+			path: "/ldp/db/query",
+			sendData: `{ "tables": ["water"] }`,
+			function: handleQuery,
+			errorstr: "cannot unmarshal string",
+		},
+		{
+			name: "simple query with dummy reslts",
+			path: "/ldp/db/query",
+			sendData: `{ "tables": [{ "schema": "folio", "tableName": "users" }] }`,
+			establishMock: func(data interface{}) error {
+				mock := data.(pgxmock.PgxPoolIface)
+				mock.ExpectQuery(`SELECT \* FROM "folio"."users"`).
+					WillReturnRows(pgxmock.NewRows([]string{"name", "email"}).
+						AddRow("mike", "mike@example.com").
+						AddRow("fiona", "fiona@example.com"))
+				return nil
+			},
+			function: handleQuery,
+			expected: `[{"email":"mike@example.com","name":"mike"},{"email":"fiona@example.com","name":"fiona"}]`,
+		},
+	}
+
 	ts := MakeDummyModSettingsServer()
 	defer ts.Close()
 	baseUrl := ts.URL
@@ -151,7 +150,7 @@ func Test_handleTables(t *testing.T) {
 	assert.Nil(t, err)
 	defer mock.Close()
 
-	for _, test := range reportingTests {
+	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			method := "GET"
 			var reader io.Reader
